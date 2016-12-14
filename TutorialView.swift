a5fef88e77ebe416becba2e1dbbb97e375916a7e
Lauren Eisdorfer <leisdorfer67@gmail.com>
2016-12-13 20:54:25 -0600

    Updated segment control switch

--- TutorialView.swift
+++ TutorialView.swift
@@ -8,11 +8,13 @@ class TutorialView: UIView{
     private let scrollView = UIScrollView()
     private let toolBar = UIToolbar()
     private let title = UILabel()
+    private let image = UIImageView()
     private let content = UILabel()
     private var toolBarSize = CGSize()
     private var segmentedControl = UISegmentedControl()
     private let setUp: TutorialSetUp
     private var contentHeight: CGFloat?
+    
     var currentPage: Int?
     var currentSegment: Int?
     
@@ -23,6 +25,7 @@ class TutorialView: UIView{
             let steps = tutorialContent?.steps.count ?? 0
             let title = tutorialContent?.sectionTitles[currentPage ?? 0] ?? ""
             let content = configureAppropriateSegment(segment: currentSegment ?? 0)
+            
             configureContent(currentTitle: title, currentContent: content)
             (backButtonTitle, nextButtonTitle) = configureToolBarButtonTitles(steps: steps)
             configureToolBar(backButtonTitle, nextButtonTitle)
@@ -32,17 +35,14 @@ class TutorialView: UIView{
     var nextSection:(Int, Int)->Void = { _ in}
     var previousSection: ()->() = { _ in}
     var pressSelector: ()->() = { _ in}
-
+    
     init (setUp: TutorialSetUp){
         self.setUp = setUp
         super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+
         currentPage = setUp.currentPage
         currentSegment = setUp.currentSegment
         
-        
-        if currentPage == nil{
-            currentPage = 0
-        }
         backgroundColor = #colorLiteral(red: 0.953121841, green: 0.9536409974, blue: 0.9688723683, alpha: 1)
         container.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.00)
         container.layer.cornerRadius = 8
@@ -50,10 +50,10 @@ class TutorialView: UIView{
         title.font = UIFont.boldSystemFont(ofSize: 14)
         content.font = UIFont.systemFont(ofSize: 14)
         content.numberOfLines = 0
-
+        
         photographyCell.configureShadow(element: container)
         configureSegmentedControl(currentPage ?? 0)
-        addSubviews([container, segmentedControl, title, content, toolBar, pageControl])
+        addSubviews([container, segmentedControl, image, title, content, toolBar, pageControl])
     }
     
     func configureContent(currentTitle: String?, currentContent: String){
@@ -67,15 +67,8 @@ class TutorialView: UIView{
         pageControl.numberOfPages = steps
         pageControl.pageIndicatorTintColor = .white
         pageControl.currentPage = currentPage ?? 0
-
     }
     
-    @objc private func segmentedControlValueChanged(){
-        let content = configureAppropriateSegment(segment: segmentedControl.selectedSegmentIndex)
-        configureContent(currentTitle: nil, currentContent: content)
-        //updateAppropriateSegment(segment: segmentedControl.selectedSegmentIndex)
-    }
-
     func configureSegmentedControl(_ currentPage: Int){
         if currentPage != 0{
             segmentedControl = UISegmentedControl(items: ["Intro", "Demo", "Practice"])
@@ -84,35 +77,27 @@ class TutorialView: UIView{
         }
     }
     
+    @objc private func segmentedControlValueChanged(){
+        let content = configureAppropriateSegment(segment: segmentedControl.selectedSegmentIndex)
+        configureContent(currentTitle: nil, currentContent: content)
+    }
+    
     func configureAppropriateSegment(segment:Int) -> String {
         setNeedsLayout()
-        
-        switch segment{
-        case SegmentControl.intro.rawValue:
-            return tutorialContent?.content[currentPage ?? 0] ?? ""
-        case SegmentControl.demo.rawValue: break
-        case SegmentControl.practice.rawValue:
-            return tutorialContent?.practice[currentPage ?? 0] ?? ""
-        default: break
+        guard let segment = SegmentControl(rawValue: segment) else { return ""}
+        switch segment {
+        case .intro: return tutorialContent?.content[currentPage ?? 0] ?? ""
+        case .demo: break
+        case .practice: return tutorialContent?.practice[currentPage ?? 0] ?? ""
         }
         return ""
-    }
-    
-    @objc private func loadNextSection(){
-        currentPage = (currentPage ?? 0) + 1
-        nextSection(currentPage ?? 0, currentSegment ?? 0)
-    }
-    
-    @objc private func loadPreviousSection(){
-        currentPage = (currentPage ?? 0) - 1
-        previousSection()
     }
     
     func configureToolBar(_ backButtonTitle: String?, _ nextButtonTitle: String?){
         let backButton = UIBarButtonItem(title: backButtonTitle, style: .plain, target: self, action: #selector(loadPreviousSection))
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
         let nextButton = UIBarButtonItem(title: nextButtonTitle, style: .plain, target: self, action: #selector(loadNextSection))
-
+        
         if nextButtonTitle == nil{
             nextButton.isEnabled = false
         } else if backButtonTitle == nil{
@@ -126,6 +111,16 @@ class TutorialView: UIView{
         }
         toolBar.barTintColor = #colorLiteral(red: 0.953121841, green: 0.9536409974, blue: 0.9688723683, alpha: 1)
         toolBar.clipsToBounds = true
+    }
+    
+    @objc private func loadNextSection(){
+        currentPage = (currentPage ?? 0) + 1
+        nextSection(currentPage ?? 0, currentSegment ?? 0)
+    }
+    
+    @objc private func loadPreviousSection(){
+        currentPage = (currentPage ?? 0) - 1
+        previousSection()
     }
     
     func configureToolBarButtonTitles(steps: Int) -> (backButtonTitle: String?, nextButtonTitle: String?){
@@ -144,14 +139,14 @@ class TutorialView: UIView{
         }
         return (backButtonTitle, nextButtonTitle)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-
+        
         let insets = UIEdgeInsets(top: 75, left: 18, bottom: 75, right: 18)
         let contentArea = UIEdgeInsetsInsetRect(bounds, insets)
         container.frame = CGRect(x: contentArea.minX, y: contentArea.minY, width: contentArea.width, height: contentArea.height)
@@ -160,9 +155,11 @@ class TutorialView: UIView{
         let segmentedWidth = contentArea.width - insets.left - insets.right
         segmentedControl.frame = CGRect(x: contentArea.midX - segmentedWidth/2, y: contentArea.minY + 20, width: segmentedWidth, height: segmentedHeight)
         
+        image.frame = CGRect(x: contentArea.minX, y: segmentedControl.frame.maxY + 8, width: contentArea.width, height: contentArea.height/2)
+        
         let titleSize = title.sizeThatFits(contentArea.size)
         title.frame = CGRect(x: contentArea.midX - titleSize.width/2, y: container.frame.minY + 40, width: titleSize.width, height: titleSize.height)
-
+        
         let contentHeight = content.sizeThatFits(contentArea.size).height
         let contentWidth = contentArea.width - insets.left - insets.right
         content.frame = CGRect(x: contentArea.midX - contentWidth/2, y: title.frame.maxY + 40, width: contentWidth, height: contentHeight)
@@ -172,7 +169,7 @@ class TutorialView: UIView{
         
         let toolBarHeight = toolBar.sizeThatFits(contentArea.size).height
         toolBar.frame = CGRect(x: contentArea.minX, y: (container.frame.maxY + bounds.maxY)/2 - toolBarHeight/2, width: contentArea.width, height: toolBarHeight)
-
+        
         scrollView.frame = CGRect(x: contentArea.minX, y: contentArea.minY, width: contentArea.size.width, height: contentArea.size.height)
     }
 }
