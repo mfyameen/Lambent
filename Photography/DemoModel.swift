b2b6d56a59f4ec3caf249868f84f6a82c18e44ce
Lauren Eisdorfer <leisdorfer67@gmail.com>
2017-02-04 19:26:38 -0600

    Added ISO images to asset folder

--- Photography/DemoModel.swift
+++ Photography/DemoModel.swift
@@ -16,8 +16,8 @@ struct DemoSettings {
     var shutterImage: String = "waterfall2.8"
     var shutterIcon: String = "shutter1.2"
     var shutterText: String = "1/2"
-    var isoImage: String? = "flower"
-    var isoIcon: String = "iso100"
+    var isoImage: String = "iso100"
+    var isoIcon: String = "isoicon100"
     var isoText: String = "100"
     var focalImage: String = "focal18"
     var focalIcon: String = "focalicon18"
@@ -76,19 +76,19 @@ public class DemoModel {
     private func configureSectionsWhenSliderValueChanged(_ sliderValue: Int) -> DemoSettings {
         switch sliderValue {
         case 3:
-            return DemoSettings(apertureImage: "fountain2.8", apertureText: "f/2.8", cameraOpeningSize: 38, cameraSensorSize: 44, shutterImage: "waterfall2.8", shutterIcon: "shutter1.2", shutterText: "1/2", isoImage: "flower", isoIcon: "iso100", isoText: "100",focalImage: "focal18", focalIcon: "focalicon18", focalText: "18mm")
+            return DemoSettings(apertureImage: "fountain2.8", apertureText: "f/2.8", cameraOpeningSize: 38, cameraSensorSize: 44, shutterImage: "waterfall2.8", shutterIcon: "shutter1.2", shutterText: "1/2", isoImage: "iso100", isoIcon: "isoicon100", isoText: "100",focalImage: "focal18", focalIcon: "focalicon18", focalText: "18mm")
         case 4:
-            return DemoSettings(apertureImage: "fountain4", apertureText: "f/4", cameraOpeningSize: 34, cameraSensorSize: 44, shutterImage: "waterfall4", shutterIcon: "shutter1.4", shutterText: "1/4", isoImage: "flower", isoIcon: "iso200", isoText: "200", focalImage: "focal25", focalIcon: "focalicon25", focalText: "25mm")
+            return DemoSettings(apertureImage: "fountain4", apertureText: "f/4", cameraOpeningSize: 34, cameraSensorSize: 44, shutterImage: "waterfall4", shutterIcon: "shutter1.4", shutterText: "1/4", isoImage: "iso200", isoIcon: "isoicon200", isoText: "200", focalImage: "focal25", focalIcon: "focalicon25", focalText: "25mm")
         case 6:
-            return DemoSettings(apertureImage: "fountain5.6", apertureText: "f/5.6", cameraOpeningSize: 24, cameraSensorSize: 44, shutterImage: "waterfall6", shutterIcon: "shutter1.8", shutterText: "1/8", isoImage: nil, isoIcon: "iso400", isoText: "400", focalImage: "focal35", focalIcon: "focalicon35", focalText: "35mm")
+            return DemoSettings(apertureImage: "fountain5.6", apertureText: "f/5.6", cameraOpeningSize: 24, cameraSensorSize: 44, shutterImage: "waterfall6", shutterIcon: "shutter1.8", shutterText: "1/8", isoImage: "iso400", isoIcon: "isoicon400", isoText: "400", focalImage: "focal35", focalIcon: "focalicon35", focalText: "35mm")
         case 8:
-            return DemoSettings(apertureImage: "fountain8", apertureText: "f/8", cameraOpeningSize: 20, cameraSensorSize: 44, shutterImage: "waterfall8", shutterIcon: "shutter1.15", shutterText: "1/15", isoImage: nil, isoIcon: "iso800", isoText: "800", focalImage: "focal45", focalIcon: "focalicon45", focalText: "45mm")
+            return DemoSettings(apertureImage: "fountain8", apertureText: "f/8", cameraOpeningSize: 20, cameraSensorSize: 44, shutterImage: "waterfall8", shutterIcon: "shutter1.15", shutterText: "1/15", isoImage: "iso800", isoIcon: "isoicon800", isoText: "800", focalImage: "focal45", focalIcon: "focalicon45", focalText: "45mm")
         case 11:
-            return DemoSettings(apertureImage: "fountain11", apertureText: "f/11", cameraOpeningSize: 16, cameraSensorSize: 44, shutterImage: "waterfall11", shutterIcon: "shutter1.30", shutterText: "1/30", isoImage: nil, isoIcon: "iso1600", isoText: "1600", focalImage: "focal55", focalIcon: "focalicon55", focalText: "55mm")
+            return DemoSettings(apertureImage: "fountain11", apertureText: "f/11", cameraOpeningSize: 16, cameraSensorSize: 44, shutterImage: "waterfall11", shutterIcon: "shutter1.30", shutterText: "1/30", isoImage: "iso1600", isoIcon: "isoicon1600", isoText: "1600", focalImage: "focal55", focalIcon: "focalicon55", focalText: "55mm")
         case 16:
-            return DemoSettings(apertureImage: "fountain16", apertureText: "f/16", cameraOpeningSize: 12, cameraSensorSize: 44, shutterImage: "waterfall16", shutterIcon: "shutter1.60", shutterText: "1/60", isoImage: nil, isoIcon: "iso3200", isoText: "3200", focalImage: "focal80", focalIcon: "focalicon80", focalText: "80mm")
+            return DemoSettings(apertureImage: "fountain16", apertureText: "f/16", cameraOpeningSize: 12, cameraSensorSize: 44, shutterImage: "waterfall16", shutterIcon: "shutter1.60", shutterText: "1/60", isoImage: "iso3200", isoIcon: "isoicon3200", isoText: "3200", focalImage: "focal80", focalIcon: "focalicon80", focalText: "80mm")
         case 22:
-            return DemoSettings(apertureImage: "fountain22", apertureText: "f/22", cameraOpeningSize: 8, cameraSensorSize: 44, shutterImage: "waterfall22", shutterIcon: "shutter1.125", shutterText: "1/125", isoImage: nil, isoIcon: "iso6400", isoText: "6400", focalImage: "focal100", focalIcon: "focalicon100",focalText: "100mm")
+            return DemoSettings(apertureImage: "fountain22", apertureText: "f/22", cameraOpeningSize: 8, cameraSensorSize: 44, shutterImage: "waterfall22", shutterIcon: "shutter1.125", shutterText: "1/125", isoImage: "iso6400", isoIcon: "isoicon6400", isoText: "6400", focalImage: "focal100", focalIcon: "focalicon100",focalText: "100mm")
         default:
             return demoSettings
         }
