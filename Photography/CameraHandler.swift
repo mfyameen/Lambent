cb839022c5b8c204191a1a43a7400dccfe398b12
Lauren Eisdorfer <leisdorfer67@gmail.com>
2017-09-22 13:11:50 -0500

    Integrate manual camera modes for ISO and shutter speed

--- Photography/CameraHandler.swift
+++ Photography/CameraHandler.swift
@@ -3,18 +3,55 @@ import AVFoundation
 
 class CameraOverlay: UIView {
     private let slider = UISlider()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    private let device: AVCaptureDevice?
+    private let section: Page
+ 
+    init(section: Page) {
+        let session = AVCaptureDevice.DiscoverySession.init(deviceTypes: [.builtInWideAngleCamera], mediaType: nil, position: .unspecified)
+        let device = session.devices.first
+        self.section = section
+        self.device = device
+        super.init(frame: CGRect.zero)
         backgroundColor = UIColor.backgroundColor()
         alpha = 0.7
+        device.map { configureSlider(forFormat: $0.activeFormat) }
         slider.addTarget(self, action: #selector(sliderMoved), for: .valueChanged)
         addSubview(slider)
     }
     
     @objc private func sliderMoved() {
-        print("Yipeee, we're moving")
-        
+        do {
+            try device?.lockForConfiguration()
+            updateExposureMode()
+            device?.unlockForConfiguration()
+        } catch {
+            print("unable to lock device due to error: \(error)")
+        }
+    }
+    
+    private func updateExposureMode() {
+        switch section {
+        case .iso:
+            let newISO = slider.value
+            device?.setExposureModeCustom(duration: device?.exposureDuration ?? CMTime(), iso: newISO, completionHandler: nil)
+        case .shutter:
+            let newShutterSpeed = CMTime(seconds: Double(slider.value), preferredTimescale: 1000000)
+            guard newShutterSpeed.seconds > device?.activeFormat.minExposureDuration.seconds ?? 0 && newShutterSpeed.seconds < device?.activeFormat.maxExposureDuration.seconds ?? 0.5 else { return }
+            device?.setExposureModeCustom(duration: newShutterSpeed, iso: device?.iso ?? device?.activeFormat.minISO ?? 0, completionHandler: nil)
+        default: return
+        }
+    }
+    
+    private func configureSlider(forFormat format: AVCaptureDevice.Format ) {
+        switch section {
+        case .iso:
+            slider.minimumValue = format.minISO
+            slider.maximumValue = format.maxISO
+        case .shutter:
+            slider.minimumValue = Float(format.minExposureDuration.seconds)
+            slider.maximumValue = Float(format.maxExposureDuration.seconds)
+        default: return
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -35,9 +72,15 @@ class CameraHandler: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         imagePicker.sourceType = .camera
-        imagePicker.cameraOverlayView = CameraOverlay(frame: CGRect(x: 0, y: 475, width: UIScreen.main.bounds.width, height: 75))
+        let cameraOverlay = CameraOverlay(section: .iso)
+        cameraOverlay.frame = CGRect(x: 0, y: 475, width: UIScreen.main.bounds.width, height: 75)
+        imagePicker.cameraOverlayView = cameraOverlay
+
         UIApplication.shared.keyWindow?.rootViewController?.present(imagePicker, animated: true, completion: nil)
-       
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
     }
     
     required init?(coder aDecoder: NSCoder) {
